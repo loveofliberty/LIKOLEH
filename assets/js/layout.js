5414ef60b26258ce8efa852e33bd29b8f669515d
Maik Schaedler <loveofliberty@gmail.com>
2026-03-07 14:00:19 +0100

    errors fixed

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -6,12 +6,28 @@ const headerContainer = document.getElementById("site-header")
 const footerContainer = document.getElementById("site-footer")
 
 if (headerContainer) {
-headerContainer.innerHTML = headerHtml
+headerContainer.outerHTML = headerHtml
 }
 
 if (footerContainer) {
 footerContainer.innerHTML = footerHtml
 }
+
+window.addEventListener("scroll", function() {
+
+const header = document.getElementById("site-header")
+
+if (!header) {
+return
+}
+
+if (window.scrollY > 50) {
+header.classList.add("scrolled")
+} else {
+header.classList.remove("scrolled")
+}
+
+})
 
 document.dispatchEvent(new CustomEvent("layoutLoaded"))
 }).catch(() => {
